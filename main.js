62eaa0a91e22689001fd36e1eeeac49c981e8501
budigk <budigk@gmail.com>
2021-06-03 21:09:56 +0700

    Get Todos List Done

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ let baseUrl = "http://localhost:3000";
 
 $(document).ready(function(){
     auth();
-    getCovidData();
 
     $('#register').click(e=> {
         e.preventDefault();
+    
         registerAccount();
     })
 
@@ -40,9 +40,18 @@ function auth(){
     $("#content-full").hide();
 
     if (localStorage.getItem("access_token")) {
+        getCovidData();
+        getTodosList();
+
         $("#navbar").show();
         $("#content-full").show();
     } else {
+        $("#inputEmail").val(""),
+        $("#inputPassword").val(""),
+
+        $("#submit").text("Sign In")
+        $("#label-login").text("Sign in to start your session")
+        $("#register").show();
         $("#login").show();
     }
 }
@@ -52,27 +61,109 @@ function registerAccount(){
     
     auth();
     $("#register").hide();
-    $("submit").text("Register")
+    $("#submit").text("Register")
+    $("#label-login").text("New User Account")
 
 }
 
 function login(){
     console.log(baseUrl)
+
+    if ($("#submit").text() == "Register") {
+        $.ajax({
+            type: "POST",
+            url: baseUrl + "/users",
+            data: {
+                email: $("#inputEmail").val(),
+                password: $("#inputPassword").val(),
+            }
+        })
+        .done(res => {
+            swal("Register Success", "Please Login", "success");
+            auth();
+        })
+        .fail(err => {
+            swal(err.responseJSON.message, "", "warning");
+            console.log(err.responseJSON.message)
+        })
+    } else {
+        $.ajax({
+            type: "POST",
+            url: baseUrl + "/users/login",
+            data: {
+                email: $("#inputEmail").val(),
+                password: $("#inputPassword").val(),
+            }
+        })
+        .done(res => {
+            console.log("--->", res)
+            localStorage.setItem("access_token", res.access_token)
+            auth();
+        })
+        .fail(err => {
+            // console.log(err)
+            swal(err.responseJSON.message, "", "warning");
+        })
+    }
+}
+
+function getTodosList() {
     $.ajax({
-        type: "POST",
-        url: baseUrl + "/users/login",
-        data: {
-            email: $("#inputEmail").val(),
-            password: $("#inputPassword").val(),
-        }
+        type: "GET",
+        url: baseUrl + "/todos",
+        headers: { access_token : localStorage.getItem("access_token")}
     })
     .done(res => {
-        console.log("--->", res)
-        localStorage.setItem("access_token", res.access_token)
-        auth();
+        $("#list-todos").empty();
+
+        console.log(res[0].title)
+        res.forEach(todo => {
+            $("#list-todos").append(`
+                <li>
+                    <!-- checkbox -->
+                    <div class="icheck-primary d-inline ml-2">
+                        <input type="checkbox" value="" name="todo1" id="todoCheck${todo.id}">
+                        <label for="todoCheck${todo.id}"></label>
+                    </div>
+                    <!-- todo text -->
+                    <span class="text">
+                        <h4>${todo.title}</h4>
+                        <p>
+                            ${todo.description}<br>
+                            Due Date : ${todo.due_date}
+                        </p>
+                    </span>
+
+                    <div class="tools">
+                        <i class="fas fa-edit"></i>
+                        <i class="fas fa-trash"></i>
+                    </div>
+                </li>
+            `)
+        });
+        // res.foreach(todo => {
+        //     $("#list-todos").append(`
+        //     <li>
+        //         <!-- checkbox -->
+        //         <div class="icheck-primary d-inline ml-2">
+        //             <input type="checkbox" value="" name="todo1" id="todoCheck1">
+        //             <label for="todoCheck1"></label>
+        //         </div>
+        //         <!-- todo text -->
+        //         <span class="text">
+        //             <h4>${todo.title}</h4>
+        //             <p>${todo.description}</p>
+        //         </span>
+
+        //         <div class="tools">
+        //             <i class="fas fa-edit"></i>
+        //             <i class="fas fa-trash"></i>
+        //         </div>
+        //     </li>
+        // `)
     })
     .fail(err => {
-        console.log(err)
+        swal(err.responseJSON.message, "", "warning");
     })
 }
 
@@ -112,6 +203,6 @@ function getCovidData(){
         `);
     })
     .fail(err => {
-        console.log(err)
+        swal(err.responseJSON.message, "", "warning");
     })
 }
