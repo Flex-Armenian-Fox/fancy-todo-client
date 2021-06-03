0818e7736188237f76786c43356ceaaf4ba25932
budigk <budigk@gmail.com>
2021-06-04 05:50:37 +0700

    Update Status, Edit dan Delete Done

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// let dateFormat = require('dateformat') ;
+// import dateFormat from 'dateformat'
 let baseUrl = "http://localhost:3000";
 
 $(document).ready(function(){
@@ -11,14 +13,6 @@ $(document).ready(function(){
 
     $("#login-form").submit(e => {
         e.preventDefault();
-        // $(document).Toasts('create', {
-        //     body: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr.',
-        //     title: 'Toast Title',
-        //     subtitle: 'Subtitle',
-        //     icon: 'fas fa-envelope fa-lg',
-        //   })
-
-        console.log("XX")
         login();
     })
 
@@ -30,6 +24,16 @@ $(document).ready(function(){
         $("#inputPassword").val(""),
 
         auth();
+    })
+
+    $("#new-todo").click(e => {
+        e.preventDefault();
+        showTodoForm(true);
+    })
+
+    $("#cancel-button").click(e => {
+        e.preventDefault();
+        standbyPage()
     })
 })
 
@@ -122,8 +126,8 @@ function getTodosList() {
                 <li>
                     <!-- checkbox -->
                     <div class="icheck-primary d-inline ml-2">
-                        <input type="checkbox" value="" name="todo1" id="todoCheck${todo.id}">
-                        <label for="todoCheck${todo.id}"></label>
+                        <input type="checkbox" value="Done" name="todo${todo.id}" id="todoCheck${todo.id}">
+                        <label for="todoCheck${todo.id}" onclick="updateStatus(${todo.id})"></label>
                     </div>
                     <!-- todo text -->
                     <span class="text">
@@ -135,11 +139,17 @@ function getTodosList() {
                     </span>
 
                     <div class="tools">
-                        <i class="fas fa-edit"></i>
-                        <i class="fas fa-trash"></i>
+                        <i class="fas fa-edit" onClick="showTodoForm(false, ${todo.id})"></i>
+                        <i class="fas fa-trash" onClick="deleteTodo(${todo.id})"></i>
                     </div>
                 </li>
             `)
+
+            if (todo.status == "Done") {
+                $("#todoCheck" + todo.id).prop('checked', true)
+            } else {
+                $("#todoCheck" + todo.id).prop('checked', false)
+            }
         });
         // res.foreach(todo => {
         //     $("#list-todos").append(`
@@ -174,6 +184,11 @@ function getCovidData(){
     })
     .done(res => {
         // console.log("DATA ==>> ", res.data["Active Cases_text"])
+        $("#active-cases").empty();
+        $("#new-cases").empty();
+        $("#total-cases").empty();
+        $("#death-cases").empty();
+
         $("#active-cases").append(`
             <h3>${res.data["Active Cases_text"]}</h3>
             <p>Active Covid Cases <br>
@@ -205,4 +220,142 @@ function getCovidData(){
     .fail(err => {
         swal(err.responseJSON.message, "", "warning");
     })
+}
+
+function showTodoForm(isNew, id) {
+    $("#navbar").show();
+    $("#todos-form").show();
+    $("#login").hide();
+    $("#content-full").hide();   
+    $("#card-title").text("Add New Data");
+
+    if (isNew == false){
+        $("#card-title").text("Edit Data");
+        $.ajax({
+            type: "GET",
+            url: baseUrl + "/todos/" + id,
+            headers: {
+                access_token: localStorage.getItem("access_token")
+            }
+        })
+        .done(res => {
+            $("#inputTitle").val(res.title)
+            $("#inputDescription").val(res.description)
+            $("#inputDueDate").val(res.due_date.slice(0, 10))
+        })
+        .fail(err => {
+            swal(err.responseJSON.message, "", "warning");
+            console.log(err.responseJSON.message)
+        })    
+    }
+
+    $("#todos-form").submit(e => {
+        e.preventDefault();
+
+        saveTodo(isNew?0:id);
+    })
+}
+
+function saveTodo(id){
+    console.log("XXXXX", id)
+    if (id == 0){
+        $.ajax({
+            type: "POST",
+            url: baseUrl + "/todos",
+            data: {
+                title: $("#inputTitle").val(),
+                description: $("#inputDescription").val(),
+                status: "Undone",
+                due_date: $("#inputDueDate").val(),
+            },
+            headers: {
+                access_token: localStorage.getItem("access_token")
+            }
+        })
+        .done(res => {
+            swal("Todo Add Success", "", "success");
+            
+            standbyPage();
+        })
+        .fail(err => {
+            swal(err.responseJSON.message, "", "warning");
+            console.log(err.responseJSON.message)
+        })
+    } else {
+        $.ajax({
+            type: "PUT",
+            url: baseUrl + "/todos/" + id,
+            data: {
+                title: $("#inputTitle").val(),
+                description: $("#inputDescription").val(),
+                due_date: $("#inputDueDate").val(),
+            },
+            headers: {
+                access_token: localStorage.getItem("access_token")
+            }
+        })
+        .done(res => {
+            swal("Todo Update Success", "", "success");
+            
+            standbyPage();
+        })
+        .fail(err => {
+            swal(err.responseJSON.message, "", "warning");
+            console.log(err.responseJSON.message)
+        })
+    }
+    
+}
+
+function standbyPage(){
+    getTodosList();
+    $("#navbar").show();
+    $("#todos-form").hide();
+    $("#login").hide();
+    $("#content-full").show();   
+}
+
+function deleteTodo(id) {
+    $.ajax({
+        type: "DELETE",
+        url: baseUrl + "/todos/" + id,
+        headers: {
+            access_token: localStorage.getItem("access_token")
+        }
+    })
+    .done(res => {
+        swal("Todo Delete Success", "", "success");
+        
+        standbyPage();
+    })
+    .fail(err => {
+        swal(err.responseJSON.message, "", "warning");
+        console.log(err.responseJSON.message)
+    })   
+}
+
+function updateStatus(id){
+    let setStatus = "";
+    if ($("#todoCheck" + id).prop('checked') == true){
+        setStatus = "Undone"
+    } else {
+        setStatus = "Done"
+    }
+
+    $.ajax({
+        type: "PATCH",
+        url: baseUrl + "/todos/" + id,
+        data: {
+            status: setStatus
+        },
+        headers: {
+            access_token: localStorage.getItem("access_token")
+        }
+    })
+    .fail(err => {
+        swal(err.responseJSON.message, "", "warning");
+        console.log(err.responseJSON.message)
+    })   
+
+    // console.log(id, $("#todoCheck" + id).val(), $("#todoCheck" + id).prop('checked'))
 }
